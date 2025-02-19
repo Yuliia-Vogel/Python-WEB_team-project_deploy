2e3964e96a136249a86feff4b528829eb1817356
Vladyslav Petrychuk <vladyslav.petrychuk42@gmail.com>
2025-02-19 14:07:05 +0200

    Add note and tags models

--- src/notes/models.py
+++ src/notes/models.py
@@ -1,4 +1,16 @@
 from django.db import models
 
-# Create your models here.
-# тестовий комміт
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.title
