53b4daec102929c5691284a4d58b1813801f7c3c
Sergstm <sstm@ukr.net>
2025-02-19 11:48:42 +0200

    test commit

--- assistant_app/asgi.py
+++ assistant_app/asgi.py
@@ -14,3 +14,5 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assistant_app.settings')
 
 application = get_asgi_application()
+
+#test comment
