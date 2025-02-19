0d82f77826ec27ab4ab2d051aa3387afed788f82
Vladyslav Petrychuk <vladyslav.petrychuk42@gmail.com>
2025-02-19 15:04:16 +0200

    Set SECRET_KEY from .env

--- src/assistant_app/settings.py
+++ src/assistant_app/settings.py
@@ -14,7 +14,7 @@ env = environ.Env()
 environ.Env.read_env(BASE_DIR / '.env')
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
