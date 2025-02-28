c6ad4463741fa37a85e4a6e7644eaaeb362668f6
KyryloDolia <fr1ghtn1ght2101@gmail.com>
2025-03-01 00:35:43 +0200

    Removed some unnecessary comments

--- src/contacts/models.py
+++ src/contacts/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-User = get_user_model()  # Використання загального підходу для користувача
-
+User = get_user_model()
 class Contact(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -15,7 +14,7 @@ class Contact(models.Model):
         User, 
         on_delete=models.CASCADE, 
         related_name='contacts',
-        null=False,  # Обмеження null значень
+        null=False,
         blank=False
     )
     
