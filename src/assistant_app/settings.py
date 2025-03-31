ddac3f17741dbcc5258c71ffe7a2a02ad94a172a
Yuliia Melnychenko <arwen.vogel@gmail.com>
2025-03-31 20:53:57 +0300

    railway added to allowed hosts

--- src/assistant_app/settings.py
+++ src/assistant_app/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["python-webteam-projectdeploy-production.up.railway.app"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
