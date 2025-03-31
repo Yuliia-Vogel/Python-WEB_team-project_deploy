fbf819b24d9da3eea648bd6a9f41d4da95cc6e31
Yuliia Melnychenko <arwen.vogel@gmail.com>
2025-03-31 23:45:19 +0300

    staticfiles folder created

--- src/assistant_app/settings.py
+++ src/assistant_app/settings.py
@@ -149,7 +149,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "templates")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "templates")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
